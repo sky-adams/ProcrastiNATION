ff2569bb3c8038fa3f5680ba74863e86f1591278
jaerickson <31251673+jaerickson@users.noreply.github.com>
2017-09-22 09:35:21 -0700

    Update 1-1.js

--- 1-1.js
+++ 1-1.js
@@ -4,23 +4,27 @@ var c = 0;
 
 $(document).ready(function(){
 	$(".ansA").click(function(){
-	a++;});
-  $(".ansB").click(function(){
+		a++;});
+	$(".ansB").click(function(){
 		b++;});
  	$(".ansC").click(function(){
 		c++;});
 	$("button").click(function(){
-  if(a > b && a > c){
-    $("#imgA").show();
-  }
-  if(b > a && b > c){
-    $("#imgB").show();
-  }
-  if(c > a && c > b){
-    $("#imgC").show();
-  }
-  if(a == b && a == c && b == c){
-    $("#imgD").show();
-  }
+	if(a > b && a > c){
+		$("#imgA").show();
+		$("#paraA").show();
+	}
+	if(b > a && b > c){
+		$("#imgB").show();
+		$("#paraB").show();
+	}
+	if(c > a && c > b){
+		$("#imgC").show();
+		$("#paraB").show();
+	}
+	if(a == b && a == c && b == c){
+		$("#imgD").show();
+		$("#paraD").show();
+	}
 	});
 });
